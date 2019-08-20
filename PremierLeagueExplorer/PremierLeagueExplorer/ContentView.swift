043fb6d5f9b1cb7f849ebcd27faf60a46f354ba8
Volodymyr <klymenkodp@gmail.com>
2019-08-19 22:31:04 -0400

    Init project⚽️

---
+++ PremierLeagueExplorer/PremierLeagueExplorer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PremierLeagueExplorer
+//
+//  Created by Volodymyr Klymenko on 2019-08-19.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ PremierLeagueExplorer/PremierLeagueExplorerTests/PremierLeagueExplorerTests.swift
@@ -0,0 +1,34 @@
+//
+//  PremierLeagueExplorerTests.swift
+//  PremierLeagueExplorerTests
+//
+//  Created by Volodymyr Klymenko on 2019-08-19.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import XCTest
+@testable import PremierLeagueExplorer
+
+class PremierLeagueExplorerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
