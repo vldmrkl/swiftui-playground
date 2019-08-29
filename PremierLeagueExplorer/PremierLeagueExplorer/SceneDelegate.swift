8f7be3bf67c6527ef51262b4fe3cee059b359aa8
Volodymyr <klymenkodp@gmail.com>
2019-08-28 20:06:19 -0400

    Rename store -> teamStore

--- PremierLeagueExplorer/PremierLeagueExplorer/SceneDelegate.swift
+++ PremierLeagueExplorer/PremierLeagueExplorer/SceneDelegate.swift
@@ -13,12 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let store = TeamStore(service: .init())
-            window.rootViewController = UIHostingController(rootView: ContentView().environmentObject(store))
+            let teamStore = TeamStore(service: .init())
+            window.rootViewController = UIHostingController(rootView: ContentView().environmentObject(teamStore))
             self.window = window
             window.makeKeyAndVisible()
         }
