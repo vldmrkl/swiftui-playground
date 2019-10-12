730f5a175389069a46ff36df719eede1382f98a2
Volodymyr <klymenkodp@gmail.com>
2019-10-12 00:56:26 -0400

    Init project

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Volodymyr Klymenko on 2019-10-12.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
